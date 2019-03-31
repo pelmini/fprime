f023e24b980bfad157057fae8fae840f94fc2429
M Starch <lestarch@googlemail.com>
2019-03-31 10:22:52 -0700

    lestarch: removing blinking test from main

--- examples/Arduino/Top/Main.cpp
+++ examples/Arduino/Top/Main.cpp
@@ -1,8 +1,5 @@
 #include <examples/Arduino/Top/Components.hpp>
-#include <Arduino.h>
 int main(int argc, char* argv[]) {
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
     constructApp();
     return 0;
 }
