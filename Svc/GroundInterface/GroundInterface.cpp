8527ec0ccebaf48ff5017b4c4ba2bb926df14992
M Starch <lestarch@googlemail.com>
2019-03-31 10:24:44 -0700

    lestarch: improved ground interface

--- Svc/GroundInterface/GroundInterface.cpp
+++ Svc/GroundInterface/GroundInterface.cpp
@@ -19,10 +19,18 @@ namespace Svc {
     GroundInterfaceComponentImpl(
         const char *const compName
     ) :
-      GroundInterfaceComponentBase(compName)
+      GroundInterfaceComponentBase(compName),
 #else
-    GroundInterfaceImpl(void)
+  GroundInterfaceComponentBase(void),
 #endif
+    m_ext_buffer(0xfeedfeed, 0xdeeddeed, reinterpret_cast<POINTER_CAST>(m_buffer), GND_BUFFER_SIZE),
+
+    // Input processing variables
+    m_state(START),
+    m_data_size(0),
+    m_remaining(0),
+    m_in_offset(0),
+    m_in_wrapper(m_in_buffer, GND_BUFFER_SIZE)
   {
 
   }
@@ -52,10 +60,19 @@ namespace Svc {
         U32 context
     )
   {
-      Fw::Buffer output;
-      output.setsize(data.getBuffLength());
-      output.setdata(reinterpret_cast<POINTER_CAST>(data.getBuffAddr()));
-      write_out(0, output);
+      // TODO: replace with a call to a buffer manager
+      Fw::Buffer buffer = m_ext_buffer;
+      Fw::ExternalSerializeBuffer buffer_wrapper(reinterpret_cast<U8*>(m_ext_buffer.getdata()),
+                                                 m_ext_buffer.getsize());
+      // Serialize data
+      buffer_wrapper.serialize(START_WORD);
+      buffer_wrapper.serialize(data.getBuffLength());
+      buffer_wrapper.serialize(data.getBuffAddr(), data.getBuffLength());
+      buffer_wrapper.serialize(static_cast<U32>(0xdeadbeef));
+
+      buffer.setsize(buffer_wrapper.getBuffLength());
+      buffer.setdata(reinterpret_cast<POINTER_CAST>(buffer_wrapper.getBuffAddr()));
+      write_out(0, buffer);
   }
 
   void GroundInterfaceComponentImpl ::
@@ -68,8 +85,121 @@ namespace Svc {
   void GroundInterfaceComponentImpl ::
     readCallback_handler(
         const NATIVE_INT_TYPE portNum,
-        Fw::Buffer &fwBuffer
+        Fw::Buffer &buffer
     )
-  {}
+  {
+      processData(buffer);
+  }
 
+  void GroundInterfaceComponentImpl ::
+    schedIn_handler(
+        const NATIVE_INT_TYPE portNum, /*!< The port number*/
+        NATIVE_UINT_TYPE context /*!< The call order*/
+    )
+  {
+      // TODO: replace with a call to a buffer manager
+      Fw::Buffer buffer = m_ext_buffer;
+      readPoll_out(0, buffer);
+      processData(buffer);
+  }
+
+  void GroundInterfaceComponentImpl ::
+    processData(Fw::Buffer& buffer)
+  {
+      U8* pointer = reinterpret_cast<U8*>(buffer.getdata());
+      // Outer loop: this portion reads the data in the incoming buffer and stops when the data
+      // runs out. Any non-processed data remains in the buffer and the remaining variable tracks
+      // how much is left-over.
+      while (pointer < (reinterpret_cast<U8*>(buffer.getdata()) + buffer.getsize())) {
+          // Detect based on state how much data needs to be read into our buffer.
+          NATIVE_UINT_TYPE read_size = 0;
+          switch (m_state) {
+              case DATA:
+                  read_size = static_cast<NATIVE_UINT_TYPE>(m_data_size);
+                  break;
+              case CHECK:
+                  read_size = sizeof(U32);
+                  break;
+              default:
+                  read_size = sizeof(TOKEN_TYPE);
+                  break;
+          }
+          // Read the data byte-by-byte into the buffer in preparation for processing
+          // of the data. At no time should the remaining data be more that the needed
+          // data from this step.
+          FW_ASSERT(m_remaining <= read_size, m_remaining, read_size);
+          while ((read_size - m_remaining > 0) &&
+                 (pointer < reinterpret_cast<U8*>(buffer.getdata()))) {
+              m_in_buffer[m_remaining + m_in_offset] = *pointer;
+              m_remaining++;
+          }
+          // Return if not enough data yet
+          if ((read_size - m_remaining) > 0) {
+              return;
+          }
+          m_in_wrapper.setBuffLen(m_in_offset + m_remaining);
+          // Process the data based on the state. If we are in the start phase, scan for the start
+          // token. In size state, read the size, data state, copy the data, and in the check state
+          // read the checksum for comparison.
+          switch(m_state) {
+              case START:
+                  TOKEN_TYPE token;
+                  m_data_size = 0;
+                  m_in_wrapper.deserialize(token);
+                  // Correct word, move on to the next state
+                  if (token == START_WORD) {
+                      m_state = SIZE;
+                      m_remaining = m_remaining - sizeof(START_WORD);
+                      m_in_offset +=  sizeof(START_WORD);
+                  }
+                  // Wrong word, shift away bad start byte
+                  else {
+                      for (NATIVE_UINT_TYPE i = 1; i < m_remaining; i++) {
+                          m_in_buffer[m_in_offset + i - 1] = m_in_buffer[m_in_offset + i];
+                      }
+                      // Three bytes left over after shift
+                      m_remaining = m_remaining - 1;
+                  }
+                  break;
+              // When looking for size, ball-park check it
+              case SIZE:
+                  m_in_wrapper.deserialize(m_data_size);
+                  m_remaining =  m_remaining - sizeof(m_data_size);
+                  // Bad size, bail and copy over erroneous data
+                  if (m_data_size >= MAX_DATA_SIZE) {
+                      m_state = START;
+                      std::memcpy(m_in_buffer, m_in_buffer + m_in_offset, sizeof(m_data_size));
+                      m_in_offset = 0;
+                      m_remaining = sizeof(m_data_size);
+                  }
+                  // Good size, keep processing
+                  else {
+                      m_state = DATA;
+                      m_in_offset += sizeof(m_data_size);
+                  }
+                  break;
+              case DATA:
+                  m_remaining -= m_data_size;
+                  m_state = CHECK;
+                  m_in_offset += m_data_size;
+                  break;
+              case CHECK:
+                  // Read the checksum
+                  U32 checksum;
+                  m_in_wrapper.deserialize(checksum);
+                  // Regardless of success, we will reset here in preparation for the next message.
+                  // Checksum failures forfeit previous data.
+                  m_state = START;
+                  m_in_offset  = 0;
+                  m_remaining =  m_remaining - sizeof(checksum);
+                  //Recalculate the checksum and ensure it is correct in order to send the data out
+                  U32 recalculate = 0xfeebdaed;
+                  if (checksum == recalculate) {
+                      Fw::ComBuffer com;
+                      com.setBuff(m_in_buffer + 2  * sizeof(TOKEN_TYPE), m_data_size);
+                      uplinkPort_out(0, com, 0);
+                  }
+          }
+      }
+  }
 } // end namespace Svc
--- Svc/GroundInterface/GroundInterface.hpp
+++ Svc/GroundInterface/GroundInterface.hpp
@@ -3,9 +3,12 @@
 // \author lestarch
 // \brief  hpp file for GroundInterface component implementation class
 // ====================================================================== 
-
+#include <Fw/Types/Serializable.hpp>
 #ifndef GroundInterface_HPP
 #define GroundInterface_HPP
+
+#define GND_BUFFER_SIZE 512
+#define TOKEN_TYPE U32
 
 #include "Svc/GroundInterface/GroundInterfaceComponentAc.hpp"
 
@@ -16,6 +19,21 @@ namespace Svc {
   {
 
     public:
+      /**
+       * ProcessingState:
+       *
+       * State of the processing of incoming data.
+       */
+      enum ProcessingState {
+          START, //! Searching for a start-bit
+          SIZE,  //! Reading 4 byte size
+          DATA,  //! Reading all data
+          CHECK, //! Reading 4 byte checksum
+          MAX_PROCESSING_STATE
+      };
+
+      static const U32 MAX_DATA_SIZE = 2048;
+      static const TOKEN_TYPE START_WORD = static_cast<TOKEN_TYPE>(0xdeadbeef);
 
       // ----------------------------------------------------------------------
       // Construction, initialization, and destruction
@@ -69,7 +87,26 @@ namespace Svc {
           Fw::Buffer &fwBuffer 
       );
 
+      //! Handler implementation for schedIn
+      //!
+      void schedIn_handler(
+          const NATIVE_INT_TYPE portNum, /*!< The port number*/
+          NATIVE_UINT_TYPE context /*!< The call order*/
+      );
 
+      //! Process a data buffer containing a read from the serial port
+      void processData(Fw::Buffer& data /*!< Data to process */);
+
+      // Output variables
+      Fw::Buffer m_ext_buffer;
+      U8 m_buffer[GND_BUFFER_SIZE];
+      // Input variables used when reading/processing
+      Fw::ExternalSerializeBuffer m_in_wrapper;
+      ProcessingState m_state; //!< Processing state for incoming data
+      NATIVE_UINT_TYPE m_remaining; //!< Data remaining in the input buffer
+      NATIVE_UINT_TYPE m_in_offset; //!< Offset into in-buffer where reading occurs
+      TOKEN_TYPE m_data_size; //!< Data size expected in incoming data
+      U8 m_in_buffer[GND_BUFFER_SIZE];
     };
 
 } // end namespace Svc
