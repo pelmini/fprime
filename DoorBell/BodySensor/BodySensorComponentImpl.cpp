eaf2cf27e658b513e533c87a762b6bcce02c391e
pelmini <vbai@go.pasadena.edu>
2019-09-07 21:02:06 -0700

    Added components via fprime templates

---
+++ DoorBell/BodySensor/BodySensorComponentImpl.cpp
@@ -0,0 +1,69 @@
+// ======================================================================
+// \title  BodySensorComponentImpl.cpp
+// \author pelmini
+// \brief  cpp file for BodySensor component implementation class
+//
+// \copyright
+// Copyright 2009-2015, by the California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged.
+//
+// ======================================================================
+
+
+#include <DoorBell/BodySensor/BodySensorComponentImpl.hpp>
+#include "Fw/Types/BasicTypes.hpp"
+
+namespace DoorBell {
+
+  // ----------------------------------------------------------------------
+  // Construction, initialization, and destruction
+  // ----------------------------------------------------------------------
+
+  BodySensorComponentImpl ::
+#if FW_OBJECT_NAMES == 1
+    BodySensorComponentImpl(
+        const char *const compName
+    ) :
+      BodySensorComponentBase(compName)
+#else
+    BodySensorComponentImpl(void)
+#endif
+  {
+
+  }
+
+  void BodySensorComponentImpl ::
+    init(
+        const NATIVE_INT_TYPE instance
+    )
+  {
+    BodySensorComponentBase::init(instance);
+  }
+
+  BodySensorComponentImpl ::
+    ~BodySensorComponentImpl(void)
+  {
+
+  }
+
+  // ----------------------------------------------------------------------
+  // Handler implementations for user-defined typed input ports
+  // ----------------------------------------------------------------------
+
+  void BodySensorComponentImpl ::
+    schedIn_handler(
+        const NATIVE_INT_TYPE portNum,
+        NATIVE_UINT_TYPE context
+    )
+  {
+    int val = 0;
+    val = analogRead(A6);
+
+    if(val > 150)
+    {
+      ON_OFF_out(0, true);
+    }
+  }
+
+} // end namespace DoorBell
---
+++ DoorBell/Light/LightComponentImpl.cpp
@@ -0,0 +1,71 @@
+// ======================================================================
+// \title  LightComponentImpl.cpp
+// \author pelmini
+// \brief  cpp file for Light component implementation class
+//
+// \copyright
+// Copyright 2009-2015, by the California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged.
+//
+// ======================================================================
+
+#include <Arudino.h>
+#include <DoorBell/Light/LightComponentImpl.hpp>
+#include "Fw/Types/BasicTypes.hpp"
+
+namespace DoorBell {
+
+  // ----------------------------------------------------------------------
+  // Construction, initialization, and destruction
+  // ----------------------------------------------------------------------
+
+  LightComponentImpl ::
+#if FW_OBJECT_NAMES == 1
+    LightComponentImpl(
+        const char *const compName
+    ) :
+      LightComponentBase(compName)
+#else
+    LightComponentImpl(void)
+#endif
+  {
+
+  }
+
+  void LightComponentImpl ::
+    init(
+        const NATIVE_INT_TYPE instance
+    )
+  {
+    LightComponentBase::init(instance);
+    pinMode(6, OUTPUT);
+  }
+
+  LightComponentImpl ::
+    ~LightComponentImpl(void)
+  {
+
+  }
+
+  // ----------------------------------------------------------------------
+  // Handler implementations for user-defined typed input ports
+  // ----------------------------------------------------------------------
+
+  void LightComponentImpl ::
+    ON_OFF_handler(
+        const NATIVE_INT_TYPE portNum,
+        bool TurnOn
+    )
+  { if(TurnOn)
+    {
+      digitalWrite(6, HIGH);
+    }
+    else
+    {
+      digitalWrite(6, LOW);
+    }
+
+  }
+
+} // end namespace DoorBell
